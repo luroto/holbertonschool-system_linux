d8c53e813dbaeae466616b134be24aa4f485d539
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-16 19:47:05 +0000

    [Fix] include guarding ls.h

--- 0x00-ls/ls.h
+++ 0x00-ls/ls.h
@@ -1,3 +1,5 @@
+#ifndef _LS_H_
+#define _LS_H_
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -14,3 +16,4 @@ int _strchr(char *s, char c);
 int denied_access(char *pathname, int ac);
 void major_trouble();
 int _strcmp(char *s1, char *s2);
+#endif
