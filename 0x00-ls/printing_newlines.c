56586e8e7baa53be327330a92c451b57963238fa
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-25 00:06:26 +0000

    [Fix] Adding additional condition when options is empty

--- 0x00-ls/printing_newlines.c
+++ 0x00-ls/printing_newlines.c
@@ -1,7 +1,14 @@
 #include "ls.h"
-
+/**
+ *printing_newlines - Code for printing newlines
+ *@aux: Node which was just printed
+ *@options: List of options collected
+ *@on : Options != NULL?
+ */
 void printing_newlines(dfileinfo_t *aux, char *options, int on)
 {
+	if (aux->next != NULL && on == 0)
+		printf("\n");
 	if (aux->next != NULL && on == 1 && _strchr(options, '1') == 1
 	    && _strchr(options, 'a') == 0)
 		printf("\n");
